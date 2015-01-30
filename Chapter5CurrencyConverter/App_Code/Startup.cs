9c2ceeeb4ffbb30ba6901100abb559de0090901c
blalocktamara <blalocktamara@gmail.com>
2015-01-29 18:36:38 -0800

    Project Currency Counter Added

---
+++ Chapter5CurrencyConverter/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chapter5CurrencyConverter.Startup))]
+namespace Chapter5CurrencyConverter
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
